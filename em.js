63ea33cee665e731d863990938098e2266dc8500
tottijbzy22 <54970161+tottijbzy22@users.noreply.github.com>
2019-09-06 05:05:51 +0200

    Create em.js

---
+++ em.js
@@ -0,0 +1,41 @@
+/* this file is the "app" file that loads the sdk and brings up the iframe */
+
+function log() {
+	console.log.apply(console, ['iframe-test'].concat(Array.prototype.slice.call(arguments)));
+}
+
+InboxSDK.load(1, 'iframe-test').then(function(sdk) {
+	sdk.Compose.registerComposeViewHandler(function(composeView) {
+		composeView.addButton({
+			title: "iframe test",
+			type: 'MODIFIER',
+			onClick: function() {
+				var iframe = document.createElement('iframe');
+				iframe.onload = function() {
+					iframe.contentWindow.postMessage("greeting", "*");
+				};
+				function modalMessageHandler(event) {
+					if (event.origin.match(/^chrome-extension:\/\//)) {
+					  //make sure that the message is coming from an extension and you can get more strict that the
+					  //extension id is the same as your public extension id
+						if (event.data === 'close') {
+							console.log('got close event from iframe');
+							//modal.close();
+						} else if (/* other event data */) {
+							
+						}
+					}
+				}
+				window.addEventListener('message', modalMessageHandler, false);
+				iframe.src = chrome.runtime.getURL('iframe.html'); //load the iframe.html that is in the extension bundle
+
+				var modal = sdk.Modal.show({
+					el: iframe
+				});
+				modal.on('destroy', function() {
+					window.removeEventListener('message', modalMessageHandler, false);
+				});
+			}
+		});
+	});
+});
